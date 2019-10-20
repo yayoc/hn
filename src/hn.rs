789ea0dd01ebab5623fa1ae766f247f99fb271c3
yayoc <n.yayoshi+github@gmail.com>
2019-10-20 22:24:57 +0900

    Use threads to make HTTP requests in parallel. However, This logic does not have a capability to handle errors.

--- src/hn.rs
+++ src/hn.rs
@@ -1,17 +1,38 @@
 extern crate reqwest;
 
 use serde::Deserialize;
-use std::ops::Try;
+use std::io::Read;
+use std::ops::DerefMut;
+use std::sync::Arc;
+use std::sync::LockResult;
+use std::sync::Mutex;
+use std::sync::MutexGuard;
+use std::thread;
 
 pub fn get_top_stories(limit: usize) -> Result<Vec<HNItem>, Box<std::error::Error>> {
-    let mut resp: Vec<i64> =
+    let mut vec: Vec<i64> =
         reqwest::get("https://hacker-news.firebaseio.com/v0/topstories.json")?.json()?;
-    let mut items: Vec<HNItem> = Vec::new();
-    resp.drain(limit..);
-    resp.iter().for_each(|id| match get_item(&id) {
-        Ok(i) => items.push(i),
-        Err(e) => println!("{:#?}", e),
-    });
+    let mut items = Vec::new();
+
+    let lock: Arc<Mutex<usize>> = Arc::new(Mutex::new(0));
+    let mut handles: Vec<thread::JoinHandle<HNItem>> = Vec::new();
+    for i in 0..limit {
+        let mut lock2 = lock.clone();
+        let vec2 = vec.clone();
+        handles.push(thread::spawn(move || {
+            let url = format!(
+                "https://hacker-news.firebaseio.com/v0/item/{}.json",
+                vec2[i],
+            );
+            let item: HNItem = reqwest::get(url.as_str()).unwrap().json().unwrap();
+            item
+        }));
+    }
+
+    for handle in handles.into_iter() {
+        let item = handle.join().unwrap();
+        items.push(item);
+    }
     Ok(items)
 }
 
@@ -25,31 +46,5 @@ pub struct HNItem {
     time: i64,
     title: String,
     r#type: String,
-    url: String,
-}
-
-impl Try for HNItem {
-    type Ok = ();
-    type Error = ();
-
-    fn into_result(self) -> Result<Self::Ok, Self::Error> {
-        unimplemented!()
-    }
-
-    fn from_error(v: Self::Error) -> Self {
-        unimplemented!()
-    }
-
-    fn from_ok(v: Self::Ok) -> Self {
-        unimplemented!()
-    }
-}
-
-fn get_item(id: &i64) -> Result<HNItem, Box<std::error::Error>> {
-    let url = format!(
-        "{}{}{}",
-        "https://hacker-news.firebaseio.com/v0/item/", id, ".json"
-    );
-    let resp: HNItem = reqwest::get(url.as_str())?.json()?;
-    Ok(resp)
+    url: Option<String>,
 }
