63a7aba6567b972ff47182c77b71e1164ee5514a
yayoc <n.yayoshi+github@gmail.com>
2019-10-26 16:55:56 +0900

    Temp scroll functionality.

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,16 @@
 
 extern crate termion;
 
+use std::cmp::{max, min};
 use std::io::{stdin, stdout, Write};
+use std::path::Component::CurDir;
 use termion::clear;
 use termion::cursor;
 use termion::event::{Event, Key};
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
-use termion::screen::*;
 use termion::screen::AlternateScreen;
-use std::path::Component::CurDir;
+use termion::screen::*;
 use webbrowser;
 
 mod hn;
@@ -21,42 +22,65 @@ struct Cursor {
 }
 
 struct App {
-//    buffer: Vec<Vec<char>>,
     stories: Vec<hn::Story>,
     cursor: Cursor,
+    row_offset: usize,
 }
 
 impl Default for App {
     fn default() -> Self {
         Self {
-//            buffer: vec![Vec::new()],
             stories: Vec::new(),
             cursor: Cursor { row: 0, column: 0 },
+            row_offset: 0,
         }
     }
 }
 
 impl App {
+    fn terminal_size() -> (usize, usize) {
+        let (cols, rows) = termion::terminal_size().unwrap();
+        (rows as usize, cols as usize)
+    }
+
     fn open(&mut self, stories: Vec<hn::Story>) {
         self.stories = stories;
         self.cursor = Cursor { row: 0, column: 0 };
+        self.row_offset = 0;
     }
 
     fn draw<T: Write>(&self, out: &mut T) {
+        let (rows, cols) = Self::terminal_size();
+
         write!(out, "{}", clear::All);
         write!(out, "{}", cursor::Goto(1, 1));
-        for s in &self.stories {
-            for c in s.title.chars() {
+
+        for i in self.row_offset..self.row_offset + rows {
+            for c in self.stories[i].title.chars() {
                 write!(out, "{}", c);
             }
-            write!(out, "\r\n");
+            let last = (self.row_offset..self.row_offset + rows).max().unwrap();
+            if i != last {
+                write!(out, "\r\n");
+            }
         }
         write!(
             out,
             "{}",
             cursor::Goto(self.cursor.column as u16 + 1, self.cursor.row as u16 + 1)
         );
+        // For debug.
+        write!(out, "{}", self.row_offset);
         out.flush().unwrap();
+    }
+
+    fn scroll(&mut self) {
+        let (rows, _) = Self::terminal_size();
+        self.row_offset = min(self.row_offset, self.cursor.row);
+        if self.cursor.row + 1 >= rows {
+            self.row_offset = max(self.row_offset, self.cursor.row + 1 - rows);
+        }
+        println!("{:#?}", self.row_offset);
     }
 
     fn open_browser(&self) {
@@ -64,24 +88,25 @@ impl App {
         match &s.url {
             Some(u) => {
                 webbrowser::open(u.as_str());
-            },
+            }
             None => {}
         }
     }
 
     fn cursor_up(&mut self) {
-        if self.cursor.row > 0{
+        if self.cursor.row > 0 {
             self.cursor.row -= 1;
         }
+        self.scroll();
     }
 
     fn cursor_down(&mut self) {
         if self.cursor.row < self.stories.len() {
             self.cursor.row += 1;
         }
+        self.scroll();
     }
 }
-
 
 fn main() {
     let stdin = stdin();
@@ -91,7 +116,7 @@ fn main() {
     stdout.flush().unwrap();
 
     let mut stories: Vec<hn::Story> = Vec::new();
-    match hn::get_top_stories(10) {
+    match hn::get_top_stories(50) {
         Ok(mut s) => stories.append(&mut s),
         Err(e) => println!("{:#?}", e),
     };
@@ -104,22 +129,22 @@ fn main() {
         match evt.unwrap() {
             Event::Key(Key::Ctrl('c')) => {
                 return;
-            },
+            }
             Event::Key(Key::Up) => {
                 app.cursor_up();
-            },
+            }
             Event::Key(Key::Char('k')) => {
                 app.cursor_up();
-            },
+            }
             Event::Key(Key::Down) => {
                 app.cursor_down();
-            },
+            }
             Event::Key(Key::Char('j')) => {
                 app.cursor_down();
-            },
+            }
             Event::Key(Key::Char('\n')) => {
                 app.open_browser();
-            },
+            }
             _ => {}
         }
         app.draw(&mut stdout);
